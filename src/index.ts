519277574498079e18942e95b68051ca231189a6
Edward Tanguay (windows) <edward@tanguay.info>
2024-02-12 12:04:52 +0100

    before zod

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,30 @@
-console.log('typescript works');
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+const port = 3311;
+
+interface IMember {
+	id: string;
+	name: string;
+}
+
+const members: IMember[] = [
+	{
+		id: "iwj33j",
+		name: "James Hatfield",
+	},
+	{
+		id: "2jjilw",
+		name: "Maryilyn Oscarton",
+	},
+];
+
+app.get("/members", (req, res) => {
+	res.json(members);
+});
+
+app.listen(port, () => {
+	console.log(`listening on http://localhost:${port}`);
+});
